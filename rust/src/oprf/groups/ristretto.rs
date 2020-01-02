1ad3743d09d2857b2a433bee6d4f93986e3e0dc7
Alex Davidson <coela@alxdavids.xyz>
2020-01-02 13:58:33 +0000

    change names

--- rust/src/oprf/groups/ristretto.rs
+++ rust/src/oprf/groups/ristretto.rs
@@ -1,70 +1,11 @@
 use curve25519_dalek::ristretto::{RistrettoPoint, CompressedRistretto};
 use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
-use super::{CyclicGroupElement,PrimeOrderGroup,Scalar};
+use super::{PrimeOrderGroup,Scalar};
 use std::io::Error;
 use super::super::super::errors::err_deserialization;
 use sha2::Sha512;
 
-const RISTRETTO_BYTE_LENGTH:usize = 32;
-
-impl CyclicGroupElement for RistrettoPoint {
-    // generator
-    fn generator() -> Self {
-        RISTRETTO_BASEPOINT_POINT
-    }
-
-    // generator_mul
-    fn generator_mul(r: Scalar) -> Self {
-        let g = RistrettoPoint::generator();
-        g.scalar_mult(r)
-    }
-
-    // byte_length
-    fn byte_length() -> usize {
-        RISTRETTO_BYTE_LENGTH
-    }
-
-    // deserialize
-    fn deserialize(buf: Vec<u8>) -> Result<Self, Error> {
-        let mut compressed = CompressedRistretto([0u8; RISTRETTO_BYTE_LENGTH]);
-        compressed.0.copy_from_slice(&buf[..RISTRETTO_BYTE_LENGTH]);
-        match compressed.decompress() {
-            Some(rp) => return Ok(rp),
-            None => return Err(err_deserialization())
-        }
-    }
-
-    // encode_to_group
-    fn encode_to_group(buf: Vec<u8>) -> Self {
-        RistrettoPoint::hash_from_bytes::<Sha512>(buf.as_slice())
-    }
-
-    // valid point
-    fn is_valid(&self) -> bool {
-        true
-    }
-
-    // is_equal
-    fn is_equal(&self, point: Self) -> bool {
-        self == &point
-    }
-
-    // add
-    fn add(&self, point: Self) -> Self {
-        self + point
-    }
-
-    // scalar_mult
-    fn scalar_mult(&self, r: Scalar) -> Self {
-        self * r
-    }
-
-    // serialize
-    fn serialize(&self) -> Vec<u8> {
-        let cmp = self.compress();
-        cmp.to_bytes().to_vec()
-    }
-}
+const RISTRETTO_BYTE_LENGTH: usize = 32;
 
 impl PrimeOrderGroup<RistrettoPoint> {
     pub fn ristretto_255() -> PrimeOrderGroup<RistrettoPoint> {
@@ -101,7 +42,7 @@ mod tests {
     use super::err_deserialization;
 
     #[test]
-    fn pog_rist_serialization() {
+    fn ristretto_serialization() {
         let pog: PrimeOrderGroup<RistrettoPoint> = PrimeOrderGroup::ristretto_255();
         let mut rng = OsRng;
         let p = RistrettoPoint::random(&mut rng);
@@ -112,7 +53,7 @@ mod tests {
     }
 
     #[test]
-    fn pog_rist_err_ser() {
+    fn ristretto_err_ser() {
         // trigger error if buffer is malformed
         let pog: PrimeOrderGroup<RistrettoPoint> = PrimeOrderGroup::ristretto_255();
         let mut rng = OsRng;
@@ -129,7 +70,7 @@ mod tests {
     }
 
     #[test]
-    fn pog_rist_point_mult() {
+    fn ristretto_point_mult() {
         let pog: PrimeOrderGroup<RistrettoPoint> = PrimeOrderGroup::ristretto_255();
         let mut rng = OsRng;
         let p = RistrettoPoint::random(&mut rng);
@@ -144,7 +85,7 @@ mod tests {
     }
 
     #[test]
-    fn pog_rist_encode_to_group() {
+    fn ristretto_encode_to_group() {
         let pog: PrimeOrderGroup<RistrettoPoint> = PrimeOrderGroup::ristretto_255();
         let buf: [u8; 32] = [0; 32];
         let p = (pog.encode_to_group)(buf.to_vec());
